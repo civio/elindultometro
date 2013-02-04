a2df4addb6993e7992674e2a033eb45fd82d1767
David Cabo <david.cabo@gmail.com>
2013-02-04 04:03:05 +0100

    Populate table with basic JSONP

--- app/app.rb
+++ app/app.rb
@@ -6,11 +6,26 @@ require 'json'
 require './app/model'
 
 class IndultometroApp < Sinatra::Base
-  
-  get '/' do
-    content_type :json
 
+  before do
+    # TODO: Improve caching with ETags http://www.sinatrarb.com/intro#Cache%20Control
+    cache_control :public, :must_revalidate, :max_age => 3600
+  end
+
+  get '/' do
     indultos = Indulto.all()
-    indultos.to_json
+    callback_name = params['callback']
+
+    if callback_name
+      response.headers['Content-Type'] = 'text/javascript; charset=utf8'
+      response.headers['Access-Control-Allow-Origin'] = '*'
+      response.headers['Access-Control-Max-Age'] = '3600'
+      response.headers['Access-Control-Allow-Methods'] = 'GET'
+
+      "#{callback_name}(#{indultos.to_json})"
+    else
+      content_type :json
+      indultos.to_json
+    end
   end
 end
