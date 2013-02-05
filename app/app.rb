f2340505cf8869e23e0675d8423432c7c1f830d9
David Cabo <david.cabo@gmail.com>
2013-02-05 03:23:20 +0100

    Add summary to API

--- app/app.rb
+++ app/app.rb
@@ -15,13 +15,25 @@ class IndultometroApp < Sinatra::Base
     redirect '/index.html'
   end
 
+  get '/api/summary' do
+    count = repository(:default).adapter.select('SELECT pardon_year, count(pardon_year) FROM pardons GROUP BY pardon_year')
+    result = {}
+    count.each do |item| 
+      result[item.pardon_year] = item.count
+    end
+    send_response(response, result.to_json, params)
+  end
+
   get '/api/pardons' do
     # TODO: Improve caching with ETags http://www.sinatrarb.com/intro#Cache%20Control
     cache_control :public, :must_revalidate, :max_age => 3600
 
     pardons = Pardon.all(:pardon_year => '2013', :fields => [:id, :pardon_date, :role, :crime])
     result = pardons.to_json
+    send_response(response, result, params)
+  end
 
+  def send_response(response, result, params)
     if params['callback']
       response.headers['Content-Type'] = 'text/javascript; charset=utf8'
       response.headers['Access-Control-Allow-Origin'] = '*'
