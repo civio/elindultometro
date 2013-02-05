6fd0011beb17d59ce8318d29493ac87e98a3a5fc
David Cabo <david.cabo@gmail.com>
2013-02-05 04:24:16 +0100

    Refactor cache headers

--- app/app.rb
+++ app/app.rb
@@ -16,22 +16,33 @@ class IndultometroApp < Sinatra::Base
   end
 
   get '/api/summary' do
+    set_cache_headers
+
     count = repository(:default).adapter.select('SELECT pardon_year, count(pardon_year) FROM pardons GROUP BY pardon_year ORDER BY pardon_year ASC')
     result = []
     count.each do |item| 
       result.push({ :year => item.pardon_year.to_i, :count => item.count })
     end
+
     send_response(response, result.to_json, params)
   end
 
   get '/api/pardons' do
-    # TODO: Improve caching with ETags http://www.sinatrarb.com/intro#Cache%20Control
-    cache_control :public, :must_revalidate, :max_age => 3600
+    set_cache_headers
 
     year = params['year'] || '2013'
     pardons = Pardon.all(:pardon_year => year, :fields => [:id, :pardon_date, :role, :crime])
     result = pardons.to_json
+    
     send_response(response, result, params)
+  end
+
+  get '/api/search' do
+  end
+
+  def set_cache_headers
+    # TODO: Improve caching with ETags http://www.sinatrarb.com/intro#Cache%20Control
+    cache_control :public, :must_revalidate, :max_age => 3600
   end
 
   def send_response(response, result, params)
