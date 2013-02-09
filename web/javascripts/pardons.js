091ee8eaf71bfe04e14c06d7fdfba7ca7bb3940c
David Cabo <david.cabo@gmail.com>
2013-02-09 02:31:28 +0100

    Introduce pills in pardon page

--- web/javascripts/pardons.js
+++ web/javascripts/pardons.js
@@ -95,6 +95,7 @@ $(function() {
     displayYear(d.year);
   }
 
+  // FIXME: hardcoded size
   var margin = {top: 20, right: 20, bottom: 30, left: 60},
       width = 800 - margin.left - margin.right,
       height = 300 - margin.top - margin.bottom;
