2f957905da72fe128593b284319db64476ed9493
David Cabo <david.cabo@gmail.com>
2015-02-12 20:35:42 +0100

    Update i18n jekyll plugin

--- web/_plugins/i18n_filter.rb
+++ web/_plugins/i18n_filter.rb
@@ -1,13 +1,16 @@
 require 'i18n'
 
-LOCALE = :es
+# To get rid of some warning message on Jekyll startup
+I18n.enforce_available_locales = false
+
+LOCALE = :es # set your locale
 
 # Create folder "_locales" and put some locale file from https://github.com/svenfuchs/rails-i18n/tree/master/rails/locale
 module Jekyll
   module I18nFilter
     # Example:
-    # {{ post.date | localize: "%d.%m.%Y" }}
-    # {{ post.date | localize: ":short" }}
+    #   {{ post.date | localize: "%d.%m.%Y" }}
+    #   {{ post.date | localize: ":short" }}
     def localize(input, format=nil)
       load_translations
       format = (format =~ /^:(\w+)/) ? $1.to_sym : format
@@ -15,7 +18,7 @@ module Jekyll
     end
 
     def load_translations
-      unless I18n::backend.instance_variable_get(:@translations)
+      if I18n.backend.send(:translations).empty?
         I18n.backend.load_translations Dir[File.join(File.dirname(__FILE__),'../_locales/*.yml')]
         I18n.locale = LOCALE
       end
